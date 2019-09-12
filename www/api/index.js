4331026ca6949cd07d345301782558935045f689
Taylor Hulsmans <thulsmans.2994@me.com>
2019-09-12 00:12:31 -0600

    nodemon for api

--- www/api/index.js
+++ www/api/index.js
@@ -2,6 +2,8 @@ const app = require('express')();
 const http = require('http').Server(app);
 const bodyParser = require('body-parser')
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 
 http.listen(3000, () => {
