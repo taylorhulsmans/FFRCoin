ae66b0eba5f91b70251dae23b785187d9553a8c7
Taylorhulsmans <thulsmans.2994@me.com>
2022-06-10 16:12:35 -0600

    rm old code

--- proto-2/contracts_old/contracts_777/ff2/DaiMock.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.7.1;
-
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-
-contract DaiMock is ERC20 {
-  constructor(uint256 initialSupply) ERC20('Silver', 'SLV') {
-    _mint(msg.sender, initialSupply);
-  }
-}
